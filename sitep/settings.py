00e979d9e0b1956fbf03a7b927eab0e1e0083755
Sarusman <sarusman@Sarusman-2.local>
2023-09-19 00:47:55 +0200

    First

--- sitep/settings.py
+++ sitep/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(local())
+django_heroku.settings(locals())

8acada0f5f6d243ccf2f4f2d41274ba4cfd7f0cb
Sarusman <sarusman@Sarusman-2.local>
2023-10-02 16:59:19 +0200

    Version Stable

--- index/models.py
+++ index/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Formation(models.Model):
-	ids=models.CharField(max_length=200)
-	titre=models.CharField(max_length=200)
-	lieu=models.CharField(max_length=200)
-	datee=models.CharField(max_length=200)
-	address=models.CharField(max_length=200)
+	ids=models.CharField(max_length=2000)
+	titre=models.CharField(max_length=2000)
+	lieu=models.CharField(max_length=2000)
+	datee=models.CharField(max_length=2000)
+	address=models.CharField(max_length=2000)
 	image=models.CharField(max_length=5000)
 
 class Projets(models.Model):
-	ids=models.CharField(max_length=200)
-	titre=models.CharField(max_length=200)
-	datee=models.CharField(max_length=200)
+	ids=models.CharField(max_length=2000)
+	titre=models.CharField(max_length=2000)
+	datee=models.CharField(max_length=2000)
 	desciption=models.CharField(max_length=5000)
 	image=models.CharField(max_length=5000)
-	langage=models.CharField(max_length=500)
+	langage=models.CharField(max_length=5000)
